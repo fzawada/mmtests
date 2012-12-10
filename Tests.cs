1a4e1e0c8d09e43869d7998171b9e4cd9e895044
Filip Zawada <fzawada@gmail.com>
2012-12-11 00:33:35 +0100

    sample

---
+++ Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test_M()
+        {
+            var sut = new Blah();
+            Assert.AreEqual(42, sut.M());
+        }
+    }
+
+    public class Blah
+    {
+        public int M()
+        {
+            return 5;
+        }
+    }
+}
